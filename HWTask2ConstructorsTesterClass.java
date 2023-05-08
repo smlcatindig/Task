41fb3651e37efe974955c85471646e9391782944
smlcatindig <smlcatindig@gmail.com>
2023-05-08 00:19:29 -0400

    second commit

---
+++ HWTask2ConstructorsTesterClass.java
@@ -0,0 +1,10 @@
+package HWProjectTask;
+
+import class17.HWTask2Constructors;
+
+public class HWTask2ConstructorsTesterClass {
+    public static void main(String[] args) {
+        HWTask2Constructors obj2=new HWTask2Constructors(36); // public
+
+    }
+}
--- Task6.java
+++ Task6.java
@@ -6,7 +6,7 @@ public class Task6 {
     public static void main(String[] args) {
         /* Write a java program to check whether a given number is prime or not? */
 
-        int number=5;
+        int number=18;
         int counter =0;
 
         for (int i = 1; i <= number; i++) {
